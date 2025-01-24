898d4b35432a848e8ced96344c2ae0d2f9e56374
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-01-24 15:26:03 +0800

    Init iP Week 2

---
+++ src/main/java/Bob.java
@@ -0,0 +1,11 @@
+public class Bob {
+    public static void main(String[] args) {
+        String name = "Bob";
+        System.out.println("________________________________");
+        System.out.println("Hello! I'm " + name + "!");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________");
+    }
+}
