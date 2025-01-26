3c7c6c6dc04765cd5240bc026e146d812f5d96c4
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-01-27 02:11:39 +0800

    Level-1

--- src/main/java/Snorlax.java
+++ src/main/java/Snorlax.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Snorlax {
+    public static void intro(String name){
+        System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        System.out.println("    Hello..... I'm " + name + ".....");
+        System.out.println("    What can I do for you?");
+        System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+    }
+
     public static void main(String[] args) {
         String name = "Snorlax";
-        System.out.println("________________________________");
-        System.out.println("Hello! I'm " + name + "!");
-        System.out.println("What can I do for you?");
-        System.out.println("________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________");
+        intro(name);
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+
+        while (!line.equals("bye")){
+            System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+            System.out.println("    " + line + ".....");
+            System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+            line = in.nextLine();
+        }
+        System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
+        System.out.println("    Bye..... see you soon....");
+        System.out.println("    zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz");
     }
 }
