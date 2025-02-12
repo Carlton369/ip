cb7657c1c404e2402b42e044166e5065bd2607c1
Yee Zhan Xian Carlton <carltonyxz@gmail.com>
2025-02-12 11:46:18 +0800

    A-Exceptions

---
+++ src/main/java/ExceptionsPackage/NoInputException.java
@@ -0,0 +1,14 @@
+package ExceptionsPackage;
+
+import UtilityPackage.Utilities;
+
+//when user enters a blank input
+public class NoInputException extends SnorlaxException {
+
+    @Override
+    public void handle() {
+        Utilities.printBorder();
+        System.out.println("     Huh? Please type something.....");
+        Utilities.printBorder();
+    }
+}
